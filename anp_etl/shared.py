eb35a25032f75bd13aaa2a1fa989146fdcef567d
Felipe Sassi <felipe.sassi@datarisk.com>
2023-01-15 13:52:41 -0300

    update code formatting with black module

--- anp_etl/shared.py
+++ anp_etl/shared.py
@@ -4,15 +4,13 @@ from pyspark.sql import SparkSession
 def get_spark_session():
 
     spark = (
-        SparkSession
-        .builder
-        .master("local[*]")
+        SparkSession.builder.master("local[*]")
         .appName("ANP-Pipeline")
         .config("spark.jars", "./jars/postgresql-42.5.1.jar")
         .config("spark.sql.sources.partitionOverwriteMode", "dynamic")
         .getOrCreate()
-        )
-        
+    )
+
     spark.sparkContext.setLogLevel("ERROR")
 
     return spark
--- tests/test_anp_pipeline.py
+++ tests/test_anp_pipeline.py
@@ -2,4 +2,4 @@ from anp_etl import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
